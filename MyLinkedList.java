655384e0b3c77ad130fdb49690b1f363a8943386
lalurestu <lalurestubagus@gmail.com>
2023-10-25 12:11:47 +0800

    first commit

---
+++ MyLinkedList.java
@@ -0,0 +1,37 @@
+package LinkedList;
+
+import algodatb2023.hewan.*;
+
+public class MyLinkedList {
+    private MyNode head = null;
+    private MyNode tail = null;
+    private Mynode current = null;
+
+    public void add(MyNode node) {
+        // jika linkedList masih kosong
+        if (head == null) {
+            head = node;
+            tail = node;
+            current = node;
+        } else {
+            tail.setNext(node);
+            tail = node;
+            node.setNext(null);
+        }
+    }
+
+    public MyNode getHead() {
+        return this.head;
+    }
+
+    public void cetak() {
+        MyNode p = this.head;
+        while (p.getNext() != null) {
+            Hewan x = p.getElemen();
+            System.out.println(x.getNama());
+            p = p.getNext();
+        }
+        Hewan x = p.getElemen();
+        System.out.println(x.getNama());
+    }
+}
